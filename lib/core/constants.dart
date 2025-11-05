320568c88e477bd5bda9b0cdfeaf95ead9313054
Widi Arrohman <58793672+widiarrohman1234@users.noreply.github.com>
2025-11-05 13:37:54 +0700

    Update constants.dart

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-  // static const baseUrl = 'https://api.ppb.widiarrohman.my.id'; //production
+  static const baseUrl = 'https://api.ppb.widiarrohman.my.id'; //production
   // static const baseUrl = 'http://10.252.111.203:1337'; //local-strapi
-  static const baseUrl = 'http://10.252.111.169:8080'; //local-shelf
+  // static const baseUrl = 'http://10.252.111.169:8080'; //local-shelf
 }
